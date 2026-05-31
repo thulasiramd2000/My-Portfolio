79b222fc52add831c5329d22b1a98774db8dfade
thulasiramd2000 <77000679+thulasiramd2000@users.noreply.github.com>
2026-05-31 14:27:39 -0500

    styling fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
     const { theme }  = useContext(ThemeContext)
 
     return (
-        <div className="footer" style={{backgroundColor: theme.secondary}}>
+        <div className="footer" style={{backgroundColor: theme.secondary,display:'none'}}>
             <p style={{color: theme.tertiary}}>
                 Made with  
                 <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
